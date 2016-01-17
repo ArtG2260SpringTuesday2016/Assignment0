8c9e9801a97c50d0da1e5d9cf566ed4dd5c7aaf2
Zhantao Peng <peng.z@husky.neu.edu>
2016-01-17 02:47:55 -0500

    Zhantao Peng

--- sketch.js
+++ sketch.js
@@ -1,7 +1,27 @@
+var N = 150;
+
+
 function setup() {
-  
+  createCanvas(600,600)
+  //background
+  background(240,222,210)
+ //rect
+ noFill()
+ rect(100, N, 400, 400)
 }
 
 function draw() {
-  
+  //ellipse
+  fill(51)
+  ellipse(mouseX,mouseY,60,50)
+  noStroke()
+}
+function mousePressed(){
+  background(240,222,210)
+  //rect
+  fill('rgb(0,255,0)')
+  rect(150, N, 400, 400)
+  N = N + 5
+  //tri
+  triangle(50, 50, 550, 50, 300, 400)
 }
