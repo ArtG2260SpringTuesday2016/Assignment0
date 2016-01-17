e68199ccd1686de24cdc0976be56076ad7fafa0a
FlyingPig13 <anotherhale.m@gmail.com>
2016-01-17 17:36:44 -0500

    Creating Dynamic Background
    Creation of an if statement with an else determining color of background
    based on mouse x position

--- sketch.js
+++ sketch.js
@@ -1,10 +1,17 @@
+// This Variable will be used to make the background return to black
+var dusk = (1000 - mouseX);
+
 function setup() {
+  createCanvas(1000, 500)
   background(0, 0, 0)
    
 }
-
+// Changes the color of the background depending on mouse x position
+// Will go towards white as the mouse approaches the center of the screen
+//and black as the x approaches 0 or 1000 from the center of 500
 function draw() {
- // background(
- //   if (MouseX < 128)
-  //      (MouseX))
+  if (mouseX < 501) {
+      background (mouseX, mouseX, mouseX)
+  } else {
+       background(dusk, dusk, dusk)  }
 }
