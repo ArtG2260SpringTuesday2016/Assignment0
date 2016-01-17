47dd9d6dcf9751bd86c1add5a435db728597beff
FlyingPig13 <anotherhale.m@gmail.com>
2016-01-17 18:11:41 -0500

    Correct Movement and Color
    The ellipse moves correctly and the background color changes properly

--- sketch.js
+++ sketch.js
@@ -1,17 +1,38 @@
-// This Variable will be used to make the background return to black
-var dusk = (1000 - mouseX);
+
 
 function setup() {
-  createCanvas(1000, 500)
+  createCanvas(1024, 500)
   background(0, 0, 0)
-   
-}
-// Changes the color of the background depending on mouse x position
-// Will go towards white as the mouse approaches the center of the screen
-//and black as the x approaches 0 or 1000 from the center of 500
+  }
+  
+
 function draw() {
-  if (mouseX < 501) {
-      background (mouseX, mouseX, mouseX)
+  
+  //This Variable will be used to initially change the background to white
+  
+  var dawn = (mouseX / 4);
+  
+  // This Variable will be used to make the background return to black
+  var dusk = ((1024 - mouseX) / 4);
+  
+  // This Variable creates the Y arc for the ellipse
+  var cycle = sin(PI * mouseX / 1024) * -500 + 500
+  
+  /* Changes the color of the background depending on mouse x position
+     Will go towards white as the mouse approaches the center of the screen
+     and black as the x approaches 0 or 1000 from the center of 500
+   */
+
+  if (mouseX <= 512) {
+      background(dawn, dawn, dawn)
   } else {
        background(dusk, dusk, dusk)  }
+       
+  stroke("orange")
+  strokeWeight(15)
+   
+  ellipse(mouseX, cycle, 100, 100 )
+  
+  
 }
+
