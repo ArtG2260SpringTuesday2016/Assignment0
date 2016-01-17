402fd4562a7e1900ec1eb4f37f7dd6010eb1df5e
AlexisDiefenbach <aodiefenbach@gmail.com>
2016-01-17 13:57:14 -0500

    Hw Change
    little gnome

--- sketch.js
+++ sketch.js
@@ -1,7 +1,17 @@
 function setup() {
-  
+
 }
 
 function draw() {
+  ellipse(46,75, 55, 55);
+  ellipse(36,75, 10, 10);
+  ellipse(56,75, 10, 10);
+  line(36, 90, 56, 90);
+ var canvas = document.getElementById("canvas");
+  var ctx = canvas.getContext("2d");
+  ctx.fillStyle = "blue";
+  ctx.fillRect(10, 10, 500, 500);
+  triangle(20, 65, 48, 10, 76, 65);
   
+ 
 }
