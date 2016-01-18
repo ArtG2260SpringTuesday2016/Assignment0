84be2a7365ed2c0c3776a48d187f4ca48c639c52
Jacqueline-G <garruto.j@husky.neu.edu>
2016-01-17 21:36:10 -0500

    Added my first version of the HW

--- sketch.js
+++ sketch.js
@@ -1,7 +1,21 @@
+var rectX = 0;
+var Y = 55;
+
 function setup() {
-  
+  createCanvas(600, 400);
+  background(0,255,0);
 }
 
 function draw() {
   
+  //rectangle
+  fill(1);
+  noStroke(255);
+  rect(rectX, mouseY, Y, Y, 20);
+  rectX = rectX + 3
+  
+  }
+function mousePressed(){
+  //newbackground
+  background(20,255,200);
 }
