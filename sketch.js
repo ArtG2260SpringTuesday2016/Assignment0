c87bc4e8e82a45aa003f4b676a51efa3f551e4d1
jillbook <jillbook@Jills-MacBook-Pro.local>
2016-01-17 22:28:36 -0500

    bubbles
    made more bubbles in background

--- sketch.js
+++ sketch.js
@@ -14,4 +14,11 @@ function draw() {
  ellipse(150,430,a-2,a-2);
  ellipse(70,100,a-8,a-8);
  ellipse(80,20,a-12,a-12);
+ 
+ ellipse(400,250,a-7,a-7);
+ ellipse(430,160,a-10,a-10);
+ ellipse(380,80,a-15,a-15);
+ ellipse(405,25,a-17,a-17);
+ ellipse(420,350,a+6,a+6);
+ ellipse(375,470,a,a);
 }
