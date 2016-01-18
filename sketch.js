2a9a0769fcf7c8dee57cc39b675bc935177f6c0c
lifangyun <li.fang@husky.neu.edu>
2016-01-18 18:33:50 -0500

    Just finished my very first assignment

--- sketch.js
+++ sketch.js
@@ -1,7 +1,1 @@
-function setup() {
-  
-}
-
-function draw() {
-  
-}
+function setup() {createCanvas(500, 500);  background('red');}function draw() {translate(width/2, height/2);rotate(PI/3.0);strokeWeight(4);stroke(51);triangle(30, 75, 58, 20, 86, 75);translate(width/4, height/4);shearX(PI/4.0);rotate(PI/3.0);strokeWeight(4);stroke(51);triangle(30, 75, 58, 20, 86, 75);  }
