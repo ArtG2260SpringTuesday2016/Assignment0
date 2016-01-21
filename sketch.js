bfa000c12fa4a3c58355eab131936f6f2b184aef
jillbook <jillbook@Jills-MacBook-Pro.local>
2016-01-20 23:12:35 -0500

    depth
    added depth by putting bubbles in front of fish, fixed variable name

--- sketch.js
+++ sketch.js
@@ -1,4 +1,5 @@
-var a = 25; //bubble size
+var bubble = 25;
+
 
 function setup() {
   createCanvas(500,500);
@@ -7,23 +8,14 @@ function setup() {
 function draw() {
   background(165,250,245);
   
-  fill(255,255,255)
-  strokeWeight(1)
- //left bubble stream
- ellipse(100,200,a-5,a-5);
- ellipse(120,275,a,a);
- ellipse(90,355,a+5,a+5);
- ellipse(150,430,a-2,a-2);
- ellipse(70,100,a-8,a-8);
- ellipse(80,20,a-12,a-12);
- 
- //right bubble stream
- ellipse(400,250,a-7,a-7);
- ellipse(430,160,a-10,a-10);
- ellipse(380,80,a-15,a-15);
- ellipse(405,25,a-17,a-17);
- ellipse(420,350,a+6,a+6);
- ellipse(375,470,a,a);
+  fill(255,255,255);
+  strokeWeight(1);
+ ellipse(120,275,bubble,bubble);
+ ellipse(70,100,bubble-8,bubble-8);
+ ellipse(80,20,bubble-12,bubble-12);
+ ellipse(400,250,bubble-7,bubble-7);
+ ellipse(430,160,bubble-10,bubble-10);
+ ellipse(405,25,bubble-17,bubble-17);
  
  //fish
  strokeWeight(0)
@@ -39,4 +31,13 @@ function draw() {
  //eye
  fill(0)
  ellipse(mouseX+20,mouseY-10,10,10)
+ 
+ fill(255,255,255)
+ strokeWeight(1)
+ ellipse(100,200,bubble-5,bubble-5);
+ ellipse(150,430,bubble-2,bubble-2);
+ ellipse(375,470,bubble,bubble);
+ ellipse(380,80,bubble-15,bubble-15);
+ ellipse(420,350,bubble+6,bubble+6);
+ ellipse(90,355,bubble+5,bubble+5);
 }
